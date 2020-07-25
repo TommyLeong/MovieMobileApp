69cdeb638b198e7701eeff3409043492fc3b39ad
Tommy Leong <tommymoleong@gmail.com>
2020-07-25 23:09:31 +0800

    Correction to endpoint

--- Src/Services/Api.js
+++ Src/Services/Api.js
@@ -1,6 +1,6 @@
 export default {
   GET_GENRE_MOVIE_LIST: '3/genre/movie/list',
   GET_ALL_MOVIES: '3/discover/movie',
-  GET_SEARCH_MOVIE: '3/search',
+  GET_SEARCH_MOVIE: '3/search/movie',
   IMAGE_DOMAIN: 'http://image.tmdb.org/t/p/w185/',
 };
